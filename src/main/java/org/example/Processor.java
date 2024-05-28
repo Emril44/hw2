18ffe673b9fff8e64e22b72591a627056e817339
M. K. <42112702+Emril44@users.noreply.github.com>
2024-05-28 18:15:49 +0300

    Set up Processor and Sender

--- src/main/java/org/example/Processor.java
+++ src/main/java/org/example/Processor.java
@@ -1,12 +1,36 @@
 package org.example;
 
+import java.net.InetAddress;
+
 public class Processor {
+    private Encryptor encryptor;
+    private Sender sender;
+
+    public Processor(Encryptor encryptor, Sender sender) {
+        this.encryptor = encryptor;
+        this.sender = sender;
+    }
+
     public void process(Message message) {
         // TODO: process message
         String command = new String(message.getMessage());
         System.out.println("Processing command: " + command);
 
         // Formulated answer ig?
-        System.out.println("OK");
+        handleResponse("Ok", message.getbUserId());
+    }
+
+    private void handleResponse(String res, int bUserID) {
+        Message resMsg = new Message(1, bUserID, res.getBytes());
+
+        new Thread(() -> {
+            try {
+                byte[] encryptedRes = encryptor.encrypt(resMsg);
+                InetAddress target = InetAddress.getByName("localhost");
+                sender.sendMessage(encryptedRes, target);
+            } catch (Exception e) {
+                System.err.println("Error in Processor's handleResponse!");
+            }
+        }).start();
     }
 }
--- src/main/java/org/example/Sender.java
+++ src/main/java/org/example/Sender.java
@@ -5,5 +5,15 @@ import java.net.InetAddress;
 public class Sender {
     public void sendMessage(byte[] message, InetAddress target) {
         // TODO: implement message sending (just output the message for now)
+        System.out.println("Sending to " + target.getHostAddress() + ": " + bytesToHex(message));
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+
+        return sb.toString();
     }
 }
