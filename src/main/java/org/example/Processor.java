dbb8b6f525bb25c168a1e3583b8787a6daa735e1
M. K. <42112702+Emril44@users.noreply.github.com>
2024-05-28 18:05:21 +0300

    Handling message in Processor

--- src/main/java/org/example/Processor.java
+++ src/main/java/org/example/Processor.java
@@ -3,5 +3,10 @@ package org.example;
 public class Processor {
     public void process(Message message) {
         // TODO: process message
+        String command = new String(message.getMessage());
+        System.out.println("Processing command: " + command);
+
+        // Formulated answer ig?
+        System.out.println("OK");
     }
 }
--- src/test/java/MessageSendTest.java
+++ src/test/java/MessageSendTest.java
@@ -1,8 +1,17 @@
+import org.example.Decryptor;
+import org.example.Encryptor;
+import org.example.Processor;
+import org.example.SampleReceiver;
 import org.junit.jupiter.api.Test;
 
 public class MessageSendTest {
+    private Encryptor encryptor;
+    private Decryptor decryptor;
+    private Processor processor;
+    private SampleReceiver receiver;
     @Test
     public void testMessageSend() throws Exception {
         // TODO: Implement test
+
     }
 }
